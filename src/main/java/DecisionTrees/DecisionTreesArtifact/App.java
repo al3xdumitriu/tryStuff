3795874f68ad3e969d56d95b52196874a0d59859
al3xdumitriu <al3xdumitriu@yahoo.com>
2015-11-01 15:44:23 +0200

    no message

--- src/main/java/DecisionTrees/DecisionTreesArtifact/App.java
+++ src/main/java/DecisionTrees/DecisionTreesArtifact/App.java
@@ -18,9 +18,9 @@ import java.util.Set;
 
 public class App {
 
-
-	Map <String, HashMap<String,ArrayList<Double>>> memoredAttr= new HashMap <String, HashMap<String,ArrayList<Double>>>();
+	Map<String, HashMap<String, ArrayList<Double>>> memoredAttr = new HashMap<String, HashMap<String, ArrayList<Double>>>();
 	String[] bestAtributesNames;
+
 	public static void main(String[] args) {
 
 		App obj = new App();
@@ -34,23 +34,24 @@ public class App {
 		BufferedReader br = null;
 		String line = "";
 		String cvsSplitBy = ",";
-		int[] contoare;
 		String[] car = { "123", "234", "543" };
 
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		Map<String, ArrayList<Double>> addedMap = new HashMap<String, ArrayList<Double>>();
+		int initialLinesNo;
+		int nrLinie;
 		try {
 
 			br = new BufferedReader(new FileReader(csvFile));
-			int nrLinie = 0;
+			initialLinesNo = 0;
 			map = new HashMap<String, Integer>();
 			while ((line = br.readLine()) != null) {
-				nrLinie++;
+				initialLinesNo++;
 
 				// use comma as separator
 				car = line.split(cvsSplitBy);
-				if(bestAtributesNames == null){
-					bestAtributesNames=new String[car.length];
+				if (bestAtributesNames == null) {
+					bestAtributesNames = new String[car.length];
 				}
 				// count the occurrences of each value
 				// for (String sequence : values) {
@@ -65,7 +66,7 @@ public class App {
 			Double result = 0.0;
 			System.out.println(map.toString());
 			for (String sequence : map.keySet()) {
-				Double frequency = (double) map.get(sequence) / (double) nrLinie;
+				Double frequency = (double) map.get(sequence) / (double) initialLinesNo;
 				result -= frequency * (Math.log(frequency) / Math.log(2));
 
 			}
@@ -78,8 +79,8 @@ public class App {
 			br.close();
 
 			br = null;
-
-			for (int i = 0; i < car.length; i++) {
+			Map<Integer,Map<String, ArrayList<Double>>> total=new HashMap<Integer,Map<String, ArrayList<Double>>>();
+			for (int i = 0; i < car.length-1; i++) {
 
 				br = new BufferedReader(new FileReader(csvFile));
 				nrLinie = 0;
@@ -116,30 +117,37 @@ public class App {
 					// calculate the entropy
 				}
 				result = 0.0;
-				System.out.println(addedMap.toString());
+				//System.out.println(addedMap.toString());
 				Iterator it = addedMap.keySet().iterator();
 				while (it.hasNext()) {
 					String sequence = it.next().toString();
-					result=0.0;
-					Double frequency = (double) ((ArrayList)addedMap.get(sequence)).get(1) / (double)((ArrayList)addedMap.get(sequence)).get(0) ;
-					result -= frequency * (Math.log(frequency) / Math.log(2));
-					 frequency = ((double) ((ArrayList)addedMap.get(sequence)).get(0)-(double) ((ArrayList)addedMap.get(sequence)).get(1)) / (double)((ArrayList)addedMap.get(sequence)).get(0) ;
-					result -= frequency * (Math.log(frequency) / Math.log(2));
-
-					System.out.println(result);
+					result = 0.0;
+					Double frequency = (double) ((ArrayList) addedMap.get(sequence)).get(1)
+							/ (double) ((ArrayList) addedMap.get(sequence)).get(0);
+					if (frequency != 0)
+						result -= frequency * (Math.log(frequency) / Math.log(2));
+					frequency = ((double) ((ArrayList) addedMap.get(sequence)).get(0)
+							- (double) ((ArrayList) addedMap.get(sequence)).get(1))
+							/ (double) ((ArrayList) addedMap.get(sequence)).get(0);
+					if (frequency != 0)
+						result -= frequency * (Math.log(frequency) / Math.log(2));
+					// ar trebui sa adaug doar valorile optime ((ArrayList)
+					addedMap.get(sequence).add(2, result);
+					//System.out.println(result);
 				}
-
-				// double ceva=0.0;
-				// ceva-=(9.0/14.0) * (Math.log(9.0/14.0) / Math.log(2));
-				// ceva-=(5.0/14.0) * (Math.log(5.0/14.0) / Math.log(2));
-				// System.out.println("Rezultat:"+ceva);
-
-				System.out.println(result);
+				total.put(i, addedMap);
+				
 				br.close();
 
 				br = null;
-			}
 
+			}
+			System.out.println(total.toString());
+			
+			//CALCULAREA GAIN-ului
+			calculateMaxGain(total, initialLinesNo);
+
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -157,23 +165,50 @@ public class App {
 		System.out.println("Done");
 	}
 
-	public static Double calculateShannonEntropy(List<String> values) {
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		// count the occurrences of each value
-		for (String sequence : values) {
-			if (!map.containsKey(sequence)) {
-				map.put(sequence, 0);
+	
+	
+	public void calculateMaxGain(Map<Integer,Map<String, ArrayList<Double>>> total,int initialLinesNo){
+		double minSumEntropy=-1;
+		Iterator it=total.keySet().iterator();
+		while(it.hasNext()){
+			int it2=(int) it.next();
+			Iterator innerMap =total.get(it2).keySet().iterator();
+			double localSumEntropy=0.0;
+			while(innerMap.hasNext()){
+				String sequence = innerMap.next().toString();
+				if((total.get(it2).get(sequence).get(total.get(it2).get(sequence).size()-1))!=0.0)
+				localSumEntropy -= (total.get(it2).get(sequence).get(0)/initialLinesNo)*(total.get(it2).get(sequence).get(total.get(it2).get(sequence).size()-1));
+				
+				
 			}
-			map.put(sequence, map.get(sequence) + 1);
+			System.out.println("localSum:"+localSumEntropy);
+			if (minSumEntropy<localSumEntropy) {
+				minSumEntropy=localSumEntropy;
+			}
+			
 		}
 
-		// calculate the entropy
-		Double result = 0.0;
-		for (String sequence : map.keySet()) {
-			Double frequency = (double) map.get(sequence) / values.size();
-			result -= frequency * (Math.log(frequency) / Math.log(2));
-		}
-		return result;
+		System.out.println("final"+minSumEntropy);
 	}
+	
+	
+//	public static Double calculateShannonEntropy(List<String> values) {
+//		Map<String, Integer> map = new HashMap<String, Integer>();
+//		// count the occurrences of each value
+//		for (String sequence : values) {
+//			if (!map.containsKey(sequence)) {
+//				map.put(sequence, 0);
+//			}
+//			map.put(sequence, map.get(sequence) + 1);
+//		}
+//
+//		// calculate the entropy
+//		Double result = 0.0;
+//		for (String sequence : map.keySet()) {
+//			Double frequency = (double) map.get(sequence) / values.size();
+//			result -= frequency * (Math.log(frequency) / Math.log(2));
+//		}
+//		return result;
+//	}
 
 }
